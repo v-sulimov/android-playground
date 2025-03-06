1995d82fb1a9cdab71d40e40baa184e601cd5258
Vitaly Sulimov <v.sulimov@fastmail.fm>
2025-03-06 13:31:46 +0300

    Add support for edge-to-edge
    Documentation: https://developer.android.com/develop/ui/views/layout/edge-to-edge

--- application/src/main/java/com/vsulimov/playground/activity/MainActivity.kt
+++ application/src/main/java/com/vsulimov/playground/activity/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.vsulimov.playground.activity
 
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.vsulimov.playground.R
@@ -18,11 +23,32 @@ class MainActivity : AppCompatActivity() {
     private lateinit var incrementFloatingActionButton: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewsById()
+        applyWindowInsets()
         subscribeToStateChanges()
         setOnClickListeners()
+    }
+
+    private fun applyWindowInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(incrementFloatingActionButton) { v, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            // Apply the insets as a margin to the view. This solution sets
+            // only the bottom, left, and right dimensions, but you can apply whichever
+            // insets are appropriate to your layout. You can also update the view padding
+            // if that's more appropriate.
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                leftMargin = insets.left
+                bottomMargin = insets.bottom
+                rightMargin = insets.right
+            }
+
+            // Return CONSUMED if you don't want want the window insets to keep passing
+            // down to descendant views.
+            WindowInsetsCompat.CONSUMED
+        }
     }
 
     private fun findViewsById() {
